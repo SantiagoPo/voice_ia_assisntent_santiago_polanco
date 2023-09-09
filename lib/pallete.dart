7705d25441f117e77535b2def2bbc27503fa540e
GG-AFTERDEATH <maicoljosue@outlook.com>
2023-09-08 20:49:12 -0500

    adding files

---
+++ lib/pallete.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Pallete {
+  static const Color mainFontColor = Color.fromRGBO(19, 65, 20, 1);
+  static const Color FirstSuggestionBoxColor = Color.fromRGBO(169, 231, 244, 1);
+  static const Color SecondSuggestionBoxColor = Color.fromRGBO(157, 202, 235, 1);
+  static const Color thirdSuggestionBoxColor = Color.fromRGBO(162, 238, 239, 1);
+  static const Color assistantCircleColor = Color.fromRGBO(209, 243, 249, 1);
+  static const Color borderColor = Color.fromRGBO(200, 200, 200, 1);
+  static const Color black = Colors.black;
+  static const Color white = Colors.white;
+}
